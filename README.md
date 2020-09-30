4d003ab96d4c4bffd0fabd21f7c4ac05c8ef218e
Oskar Korczak <oskarissimus@gmail.com>
2020-10-01 00:20:54 +0200

    hello world

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ ssh-keygen -t rsa -b 4096 -C "mail@example.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 ``` 
+
+## terraform install
+```
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+```
---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  credentials = ".credentials.json"
+  project     = "huta99"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name   = "terraform-network"
+  auto_create_subnetworks = "true"
+}
